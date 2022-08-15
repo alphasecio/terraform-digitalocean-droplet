b07b3a1258c8865a8f844c16a25c6a58e422338d
Alphasec <95212748+alphasecio@users.noreply.github.com>
2022-08-15 15:24:13 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,21 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "> 2.8.0"
+    }
+  }
+}
+
 variable "do_token" {}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_tag" "ENV_example" {
-  name = "ENV:example"
-}
-
-resource "digitalocean_tag" "ROLE_web" {
-  name = "ROLE:web"
-}
-
-module "web" {
-  source = "../../"
-
-  droplet_count = 1
-
-  droplet_name       = "example-web"
-  droplet_size       = "nano"
-  monitoring         = true
-  private_networking = true
-  ipv6               = true
-  floating_ip        = true
-  block_storage_size = 5
-  tags               = [digitalocean_tag.ENV_example.id, digitalocean_tag.ROLE_web.id]
-  user_data          = file("user-data.web")
+resource "digitalocean_droplet" "web" {
+  image   = "ubuntu-20-04-x64"
+  name    = "tfc-1"
+  region  = "nyc1"
+  size    = "s-1vcpu-1gb"
 }
